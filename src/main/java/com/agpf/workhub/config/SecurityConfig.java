686fe4e96d6ded4e8bbebc509358ec29322bc894
AndrewGPFranco <andrewgomes1328@gmail.com>
2026-09-22 23:28:31 -0300

    feat: add exposedHeaders

--- src/main/java/com/agpf/workhub/config/SecurityConfig.java
+++ src/main/java/com/agpf/workhub/config/SecurityConfig.java
@@ -58,7 +58,9 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .exposedHeaders("Content-Disposition");
     }
 
 }
